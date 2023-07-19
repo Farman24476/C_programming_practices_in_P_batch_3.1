20f5241471c3ff518b99a898fa8363900f163fdf
MD.FARMAN ALI <108406879+Farman24476@users.noreply.github.com>
2023-07-19 14:33:46 +0800

    module 1 all

---
+++ module 1/input/inputinc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int rahim=10;
+float karim=12.12;
+char habib='H';
+printf("%d,%0.2f,%c",rahim,karim,habib);
+return 0;
+
+
+}
---
+++ module 1/print/print.c
@@ -0,0 +1,10 @@
+#include<stdint.h>
+int main()
+{
+printf("Hello World\n");
+printf("I am a new programmer\n");
+printf("I want to create a print file");
+return 0;
+
+
+}
